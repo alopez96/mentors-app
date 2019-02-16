c2e73d96e8677bdc74ebba6da309a6f73e7a1970
Arturo <alopez96@ucsc.edu>
2019-02-15 16:55:56 -0800

    create post connected to backend

--- components/CreateEvent/CreateEvent.js
+++ components/CreateEvent/CreateEvent.js
@@ -1,38 +1,76 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import CardComponent from '../Card/CardComponent';
-import {Container, Content, Header,Form, Item, Input, Icon } from 'native-base';
+import {Container, Content, Header,Form, Item,
+    Textarea, Input, Icon , Button} from 'native-base';
+import {connect} from 'react-redux';
 import {localhost} from '../../localhost';
 
-export default class CreateEvent extends Component {
+class CreateEvent extends Component {
 
 
   constructor(props){
     super(props);
     this.state = {  
         eventid: '',
-        postImg: '',
-        postDescription: '',
+        title: '',
+        imageurl: '',
+        description: '',
     };
   }
 
+  cancelCreateEvent = () => {
+    this.props.navigation.navigate('Home')
+  }
+
   createEventAction = () => {
-      console.log('create event action')
+    console.log('create event action')
+    fetch('http://'+localhost+':3000/createEvent', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        title: this.state.title,
+        description: this.state.description,
+        userid: this.props.user[0].id      
+        })
+    })
+    .then(response => response.json())
+        .then(post => {
+        if(post){
+            console.log('postid', post)
+            this.props.navigation.navigate('Home')
+        }
+        })
+        .catch( err => console.log(err));
   }
 
   render() {
+      var { title, description } = this.state;
     return (
         <Container>
-        <Header />
         <Content>
           <Form>
             <Item>
-              <Input placeholder="title" />
+              <Input placeholder="title"
+                        label='title'
+                        onChangeText={(title) => this.setState({ title })}
+                        value={title}/>
             </Item>
-            <Item>
-              <Input placeholder="Description" />
-            </Item>
+            <Textarea rowSpan={5} bordered placeholder="description"
+                label="description" onChangeText={(description) => this.setState({ description })} 
+                value={description}/>
           </Form>
+
+          <View style={styles.buttons}>
+                <TouchableOpacity onPress={() => this.cancelCreateEvent()}>
+                    <Icon name="ios-close-circle-outline"
+                    style={styles.button}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.createEventAction() }>
+                    <Icon name="ios-checkbox-outline"
+                    style={styles.button}/>
+                </TouchableOpacity>
+            </View>
         </Content>
       </Container>
     );
@@ -40,9 +78,28 @@ export default class CreateEvent extends Component {
 }
 
 
+const mapStateToProps = (state) => {
+    return {
+        user: state
+    }
+}
+
+export default connect(mapStateToProps)(CreateEvent);
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
   },
+  buttons:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+},
+button:{
+    color:'black', 
+    fontSize:40,
+    margin: 50
+}
 })
