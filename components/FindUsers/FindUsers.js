76f3b4c5d2e8345d7be304160eaaa6f9dc054544
Arturo <alopez96@ucsc.edu>
2019-02-23 18:34:32 -0800

    finding and retrieving list of users

--- components/FindUsers/FindUsers.js
+++ components/FindUsers/FindUsers.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text,  View} from 'react-native';
+import {connect} from 'react-redux';
 
 class FindUsers extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+        uids: [],
+    }
+}
+
+  componentDidMount(){
+    const { uids } = this.state;
+    console.log('findUsers props from store', this.props.users[0])
+    this.props.users[0].map(function (users) {
+      uids.push(users.id)
+    })
+}
 
   render() {    
     return (
       <View style={styles.container}>
-          <Text>
-              Find Users
-          </Text>
+          {this.props.users[0].map((user) => {
+            return(
+            <Text>
+              {user.name}
+            </Text>
+            )
+          })}
       </View>
+
     );
   }
 }
 
 
-export default FindUsers;
+const mapStateToProps = (state) => {
+  return {
+      users: state.users
+  }
+}
+
+export default connect(mapStateToProps)(FindUsers);
+
 
 const styles = StyleSheet.create({
   container: {
